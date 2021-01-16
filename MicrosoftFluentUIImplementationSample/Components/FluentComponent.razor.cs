79339dbccf9ac5b2e6718ad18934d472f838f01d
Maryam Heydarian <maryamhdr76@gmail.com>
2021-01-16 14:49:31 +0330

    Add project files.

---
+++ MicrosoftFluentUIImplementationSample/Components/FluentComponent.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MicrosoftFluentUIImplementationSample.Components
+{
+    public partial class FluentComponent
+    {
+        [Parameter]
+        public bool IsDisabled { get; set; }
+        public int Counter { get; set; }
+
+        public void IncreaseCounter()
+        {
+            Counter++;
+        }
+    }
+}
---
+++ MicrosoftFluentUIImplementationSample/Pages/Index.razor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicrosoftFluentUIImplementationSample.Pages
+{
+    public partial class Index
+    {
+        public bool IsComponentDisabled { get; set; }
+
+        protected override void OnInitialized()
+        {
+            IsComponentDisabled = false;
+            base.OnInitialized();
+        }
+
+        public void ToggleComponentDisability()
+        {
+            IsComponentDisabled = !IsComponentDisabled;
+        }
+    }
+}
